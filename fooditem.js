5203d9a1ffcafde9d0b024447794e6eb6768971c
harshkore21 <harshkoreofficial@gmail.com>
2024-07-12 16:43:26 +0530

    restaurant

---
+++ fooditem.js
@@ -0,0 +1,381 @@
+// fooditem.js
+const foodItem= [
+{
+    id: 1,
+    name: 'Paplet Thali',
+    category : 'thali',
+    rating : 9.9,
+    price: 350,
+    img: 'images/thali/papletthali.jpg',
+    quantity: 1
+},    
+{
+    id: 2,
+    name: 'Surmai Thali',
+    category : 'thali',
+    rating : 9.9,
+    price: 310,
+    img: 'images/thali/surmaithali.jpg',
+    quantity: 1
+},
+{
+    id: 3,
+    name: 'Prawns Thali',
+    category : 'thali',
+    rating : 9.5,
+    price: 330,
+    img: 'images/thali/prawnsthali.jpg',
+    quantity: 1
+},
+{
+    id: 4,
+    name: 'Bombil Thali',
+    category : 'thali',
+    rating : 9.5,
+    price: 280,
+    img: 'images/thali/bombilthali.png',
+    quantity: 1
+},
+{
+    id: 5,
+    name: 'Crab Thali',
+    category : 'thali',
+    rating : 9.3,
+    price: 330,
+    img: 'images/thali/crabthali.jpg',
+    quantity: 1
+},
+{
+    id: 6,
+    name: 'Chicken Thali',
+    category : 'thali',
+    rating : 9.9,
+    price: 290,
+    img: 'images/thali/chickenthali.jpg',
+    quantity: 1
+},
+{
+    id: 7,
+    name: 'Dry Chicken Thali',
+    category : 'thali',
+    rating : 9.7,
+    price: 299,
+    img: 'images/thali/drychickenthali.jpg',
+    quantity: 1
+},
+{
+    id: 8,
+    name: 'Mutton Thali',
+    category : 'thali',
+    rating : 9.3,
+    price: 320,
+    img: 'images/thali/muttonthali.jpg',
+    quantity: 1
+},
+{
+    id: 9,
+    name: 'Paplet Fry',
+    category : 'fishfry',
+    rating : 9.8,
+    price: 300,
+    img: 'images/products/product-3.jpg',
+    quantity: 1
+},
+{
+    id: 10,
+    name: 'Bombil Fry',
+    category : 'fishfry',
+    rating : 9.8,
+    price: 190,
+    img: 'images/fishfry/bombilfry.jpg',
+    quantity: 1
+},
+{
+    id: 11,
+    name: 'Surmai Fry',
+    category : 'fishfry',
+    rating : 9.8,
+    price: 330,
+    img: 'images/fishfry/surmaifry.jpg',
+    quantity: 1
+},
+{
+    id: 12,
+    name: 'Prawns Fry',
+    category : 'fishfry',
+    rating : 9.6,
+    price: 300,
+    img: 'images/fishfry/prawnsfry.jpg',
+    quantity: 1
+},
+{
+    id: 13,
+    name: 'Bangda Fry',
+    category : 'fishfry',
+    rating : 9.6,
+    price: 180,
+    img: 'images/fishfry/bangdafry.jpg',
+    quantity: 1
+},
+{
+    id: 14,
+    name: 'Mandeli Fry',
+    category : 'fishfry',
+    rating : 9.5,
+    price: 180,
+    img: 'images/fishfry/mandelifry.jpg',
+    quantity: 1
+},
+{
+    id: 15,
+    name: 'Malvan Samudra Fry',
+    category : 'fishfry',
+    rating : 9.9,
+    price: 650,
+    img: 'images/gallery/g-1.jpg',
+    quantity: 1
+},
+ 
+{
+    id: 18,
+    name: 'Paplet Curry',
+    category : 'gravy',
+    rating : 9.3,
+    price: 320,
+    img: 'images/gravy/papletcurry.jpg',
+    quantity: 1
+},
+{
+    id: 19,
+    name: 'Green Paplet Curry',
+    category : 'gravy',
+    rating : 9.8,
+    price: 360,
+    img: 'images/gravy/greenpaplet.jpg',
+    quantity: 1
+},
+{
+    id: 20,
+    name: 'Prawns Masala',
+    category : 'gravy',
+    rating : 9.3,
+    price: 320,
+    img: 'images/gravy/prawnsms.jpg',
+    quantity: 1
+},
+{
+    id: 21,
+    name: 'Prawns Dry',
+    category : 'gravy',
+    rating : 9.8,
+    price: 340,
+    img: 'images/gravy/prawnsdry.jpg',
+    quantity: 1
+},
+{
+    id: 22,
+    name: 'Crab Masala',
+    category : 'gravy',
+    rating : 9.3,
+    price: 300,
+    img: 'images/gravy/crabms.jpeg',
+    quantity: 1
+},
+{
+    id: 23,
+    name: 'Tisrya Masala',
+    category : 'gravy',
+    rating : 9.5,
+    price: 310,
+    img: 'images/gravy/tisryams.jpg',
+    quantity: 1
+},
+{
+    id: 24,
+    name: 'Bangda Curry',
+    category : 'gravy',
+    rating : 9.3,
+    price: 190,
+    img: 'images/gravy/bangdacurry.jpg',
+    quantity: 1
+
+},
+{
+    id: 25,
+    name: 'Bombil Masala',
+    category : 'gravy',
+    rating : 9.3,
+    price: 190,
+    img: 'images/gravy/bombilms.jpeg',
+    quantity: 1
+},
+{
+    id: 26,
+    name: 'Surmai Masala',
+    category : 'gravy',
+    rating : 9.7,
+    price: 330,
+    img: 'images/gravy/surmaims.jpg',
+    quantity: 1
+
+},
+{
+    id: 27,
+    name: 'Paplet Koliwada',
+    category : 'koliwada',
+    rating : 9.7,
+    price: 320,
+    img: 'images/koliwada/papletk.jpg',
+    quantity: 1
+
+},
+{
+    id: 28,
+    name: 'Paneer Koliwada',
+    category : 'koliwada',
+    rating : 8.9,
+    price: 230,
+    img: 'images/koliwada/paneerk.jpg',
+    quantity: 1
+},
+{
+    id: 29,
+    name: 'Surmai Koliwada',
+    category : 'koliwada',
+    rating : 9.1,
+    price: 340,
+    img: 'images/koliwada/surmaik.jpg',
+    quantity: 1
+},
+{
+    id: 30,
+    name: 'Bangda Koliwada',
+    age: '32',
+    category : 'koliwada',
+    rating : 9.1,
+    price: 200,
+    img: 'images/koliwada/bangdak.jpg',
+    quantity: 1
+},
+{
+    id: 31,
+    name: 'Prawns Koliwada',
+    category : 'koliwada',
+    rating : 9.9,
+    price: 320,
+    img: 'images/koliwada/prawnsk.jpeg',
+    quantity: 1
+},
+{
+    id: 32,
+    name: 'Boiler Chicken Handi',
+    category : 'chicken',
+    rating : 9.8,
+    price: 300,
+    img: 'images/chicken/chickenhandi.jpg',
+    quantity: 1
+},
+{
+    id: 33,
+    name: 'Green Chicken Handi',
+    category : 'chicken',
+    rating : 9.9,
+    price: 360,
+    img: 'images/chicken/greenchickenhandi.jpg',
+    quantity: 1
+},
+{
+    id: 34,
+    name: 'Special Chicken Wada',
+    category : 'chicken',
+    rating : 9.9,
+    price: 300,
+    img: 'images/chicken/chickenwada.jpg',
+    quantity: 1
+},
+{
+    id: 35,
+    name: 'Chicken Dry Liver',
+    category : 'chicken',
+    rating : 9.3,
+    price: 250,
+    img: 'images/chicken/chickenliver.jpg',
+    quantity: 1
+},
+{
+    id: 36,
+    name: 'Chicken Masala',
+    category : 'chicken',
+    rating : 9.7,
+    price: 220,
+    img: 'images/chicken/chickenms.jpg',
+    quantity: 1
+},
+{
+    id: 37,
+    name: 'Chicken Dry',
+    category : 'chicken',
+    rating : 9.9,
+    price: 240,
+    img: 'images/chicken/chickendry.jpg',
+    quantity: 1
+},
+{
+    id: 38,
+    name: 'Mutton Biryani',
+    category : 'biryani',
+    rating : 9.3,
+    price: 340,
+    img: 'images/biryani/muttonbiryani.jpg',
+    quantity: 1
+},
+{
+    id: 39,
+    name: 'Egg Biryani',
+    category : 'biryani',
+    rating : 9.5,
+    price: 200,
+    img: 'images/biryani/eggbiryani.jpg',
+    quantity: 1
+},
+{
+    id: 16,
+    name: 'Prawns Biryani',
+    category : 'biryani',
+    rating : 9.3,
+    price: 340,
+    img: 'images/biryani/prawnsbiryani.jpg',
+    quantity: 1
+},
+{
+    id: 40,
+    name: 'Solkadi',
+    category : 'biryani',
+    rating : 9.9,
+    price: 40,
+    img: 'images/biryani/solkadi.jpg',
+    quantity: 1
+},
+{
+    id: 41,
+    name: 'Kokam Sarbat',
+    category : 'biryani',
+    rating : 9.8,
+    price: 20,
+    img: 'images/biryani/kokamsarbat.jpg',
+    quantity: 1
+},
+{
+    id: 17,
+    name: 'Chicken Biryani',
+    category : 'biryani',
+    rating : 9.8,
+    price: 290,
+    img: 'images/biryani/chickenbiryani.jpg',
+    quantity: 1
+},
+
+
+]
+
+export {foodItem};
